5c0562e5392ed0653f23805d47f81b4b5f9444ae
unknown <skn21264@gmail.com>
2022-03-03 09:20:23 +0530

    03codepush

--- src/components/carousal/Carousal.js
+++ src/components/carousal/Carousal.js
@@ -44,8 +44,8 @@ export default class Carousal extends React.Component {
           <div className="team-box mt-0">
             <img src="assets/images/team-img-box.png" alt="" />
             <div className="team-content">
-              <img src="assets/images/team1.png" alt="" />
-              <h4>Dee Norris</h4>
+              <img src="assets/images/DEE.png" alt="" />
+              <h4>DEE</h4>
               <p>CEO </p>
             </div>
           </div>
@@ -54,8 +54,8 @@ export default class Carousal extends React.Component {
           <div className="team-box mt-0">
             <img src="assets/images/team-img-box.png" alt="" />
             <div className="team-content">
-              <img src="assets/images/team1.png" alt="" />
-              <h4>Dee Norris</h4>
+              <img src="assets/images/APRIL.png" alt="" />
+              <h4>APRIL</h4>
               <p>CEO </p>
             </div>
           </div>
@@ -64,8 +64,8 @@ export default class Carousal extends React.Component {
           <div className="team-box mt-0">
             <img src="assets/images/team-img-box.png" alt="" />
             <div className="team-content">
-              <img src="assets/images/team1.png" alt="" />
-              <h4>Dee Norris</h4>
+              <img src="assets/images/BRIAN.png" alt="" />
+              <h4>BRIAN</h4>
               <p>CEO </p>
             </div>
           </div>
@@ -74,8 +74,8 @@ export default class Carousal extends React.Component {
           <div className="team-box mt-0">
             <img src="assets/images/team-img-box.png" alt="" />
             <div className="team-content">
-              <img src="assets/images/team1.png" alt="" />
-              <h4>Dee Norris</h4>
+              <img src="assets/images/CHING.png" alt="" />
+              <h4>CHING</h4>
               <p>CEO </p>
             </div>
           </div>
@@ -84,8 +84,8 @@ export default class Carousal extends React.Component {
           <div className="team-box mt-0">
             <img src="assets/images/team-img-box.png" alt="" />
             <div className="team-content">
-              <img src="assets/images/team1.png" alt="" />
-              <h4>Dee Norris</h4>
+              <img src="assets/images/DAVID.png" alt="" />
+              <h4>DAVID</h4>
               <p>CEO </p>
             </div>
           </div>
@@ -94,8 +94,158 @@ export default class Carousal extends React.Component {
           <div className="team-box mt-0">
             <img src="assets/images/team-img-box.png" alt="" />
             <div className="team-content">
-              <img src="assets/images/team1.png" alt="" />
-              <h4>Dee Norris</h4>
+              <img src="assets/images/GERALT.png" alt="" />
+              <h4>GERALT</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/GUSTO.png" alt="" />
+              <h4>GUSTO</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/HERO.png" alt="" />
+              <h4>HERO</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/KOOL.png" alt="" />
+              <h4>KOOL</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/LIAM.png" alt="" />
+              <h4>LIAM</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/LUCKY.png" alt="" />
+              <h4>LUCKY</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/MATT.png" alt="" />
+              <h4>MATT</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/PEABODY.png" alt="" />
+              <h4>PEABODY</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/PHIL.png" alt="" />
+              <h4>PHIL</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/RAY.png" alt="" />
+              <h4>RAY</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/SCOTT.png" alt="" />
+              <h4>SCOTT</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/SEMI.png" alt="" />
+              <h4>SEMI</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/TAJ.png" alt="" />
+              <h4>TAJ</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/TINAS_HUBBY.png" alt="" />
+              <h4>TINAS_HUBBY</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/TONY.png" alt="" />
+              <h4>TONY</h4>
+              <p>CEO </p>
+            </div>
+          </div>
+          </div>
+        <div>
+          <div className="team-box mt-0">
+            <img src="assets/images/team-img-box.png" alt="" />
+            <div className="team-content">
+              <img src="assets/images/Z.png" alt="" />
+              <h4>Z</h4>
               <p>CEO </p>
             </div>
           </div>
